ba7cf3fa7b48d1ec27ee5b4f74cfc5b6fcea692c
achilles schmelzer <achilles@daopanel.xyz>
2022-06-06 19:36:21 -0700

    Fix Vercel env var

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,6 +2,6 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  environment: process.env.VERCEL_GIT_COMMIT_REF,
+  environment: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF,
   tracesSampleRate: 1.0,
 });

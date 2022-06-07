36dc3594084654c4fde2ee90c2ae01bbdc213329
achilles schmelzer <achilles@daopanel.xyz>
2022-06-06 19:25:15 -0700

    One more

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.VERCEL_GIT_COMMIT_REF,
   tracesSampleRate: 1.0,
 });

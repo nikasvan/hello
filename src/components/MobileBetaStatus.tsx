47dce93e9cbab1d88a64f751c28fcb36f2ac7106
nikasvan <svanidze.nikoloz@gmail.com>
2022-06-10 17:32:54 +0400

    daopanel and github icons added

--- src/components/MobileBetaStatus.tsx
+++ src/components/MobileBetaStatus.tsx
@@ -1,8 +1,24 @@
 import styled from 'styled-components';
+import Image from 'next/image';
+import Github from './Github';
 
 export default function MobileBetaStatus() {
   return (
     <BottomRight>
+      <a href="https://www.daopanel.com/" target="_blank" rel="noreferrer">
+        <Image
+          src="/assets/images/whiteonly.png"
+          width="24"
+          height="24"
+          alt="white-logo"
+        />
+      </a>
+      <a
+        href="https://www.github.com/daopanel"
+        target="_blank"
+        rel="noreferrer">
+        <Github />
+      </a>
       <PublicBeta href="https://docs.xmtp.org/" target="_blank">
         public beta
       </PublicBeta>
@@ -24,4 +40,14 @@ const BottomRight = styled.div`
   position: absolute;
   bottom: 24px;
   right: 24px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  & > a:first-child {
+    opacity: 0.12;
+  }
+  & > a {
+    margin-right: 10px;
+  }
 `;

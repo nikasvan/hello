bd42d95ac7d3d2274ed684e56ca13e328503ca2b
achilles schmelzer <100799178+killthebuddh4@users.noreply.github.com>
2022-06-14 20:11:17 -0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 const Header = (): JSX.Element => {
   return (
     <Head>
-      <title>Daopanel</title>
+      <title>daopanel</title>
       <link
         rel="apple-touch-icon"
         sizes="180x180"

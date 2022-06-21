584f29e894b12cc5402be6e820b886d306523141
achilles schmelzer <achilles@daopanel.com>
2022-06-20 21:23:43 -0700

    Add prisma generate to build step

--- src/pages/api/metrics/initEvent.ts
+++ src/pages/api/metrics/initEvent.ts
@@ -18,15 +18,13 @@ export default async function handler(
 const handlePost = async (req: NextApiRequest, res: NextApiResponse) => {
   const data = JSON.parse(req.body);
   try {
-    const result = await prisma.initEvent.create({
+    await prisma.initEvent.create({
       data: {
         address_hash: data.addressHash,
       },
     });
-    console.log(result);
     res.status(200).json(true);
   } catch (err) {
-    console.error(err);
     res.status(500).json(false);
   }
 };
